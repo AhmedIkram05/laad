765f64383dd1f7bd024efac5977a16ff34da7acb
2579986 <149397608+2579986@users.noreply.github.com>
2026-03-24 19:19:05 +0000

    added search bar box and title

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
+/* Import Styles */
+import './Dashboard.css'
+
+
 function Dashboard() {
   return (
+    <>
 
-    <div>
-      <h1>Anomalies Detected</h1>
-      <p>Placeholder text.</p>
-    </div>
+      {/* Search and Filter Bar */}
+      <div className="searchContainer">
+        <p>Search Bar</p>
+      </div>
 
+      {/* Main Page Content */}
+      <div className="mainContainer">
+
+        <div>
+          <h1>Anomalies Detected</h1>
+          <p>Placeholder text.</p>
+        </div>
+
+      </div>
+
+    </>
   );
 }
 
