88332ca8228404073e312a5b3ac45ff61782173d
2579986 <149397608+2579986@users.noreply.github.com>
2026-03-24 13:57:23 +0000

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
+/* import libraries */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* import pages */
+import MainLayout from "./layouts/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Starred from "./pages/Starred";
+import Completed from "./pages/Completed";
+import AnomalyDetails from "./pages/AnomalyDetails";
+
+
 function App() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Placeholder.</p>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+
+        {/* default opening to dashboard page */}
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+
+        {/* set main layout to all pages */}
+        <Route element={<MainLayout/>}>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/starred" element={<Starred/>}/>
+          <Route path="/completed" element={<Completed/>}/>
+          <Route path="/anomaly/:id" element={<AnomalyDetails/>}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
+
 
 export default App;
