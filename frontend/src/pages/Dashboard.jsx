dc5850b7156a57a7e374ac2efd5bac587dd01b7e
2579986 <149397608+2579986@users.noreply.github.com>
2026-03-25 21:55:45 +0000

    updated typo and search

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 
 /* Import Components */
-//import SearchBar from "../components/SearchBar";
 import AnomalyCard from "../components/AnomalyCard";
 
 /* Import Styles */
@@ -13,37 +12,12 @@ function Dashboard() {
   const [search, setSearch] = useState("");
 
   const anomalyList = [
-  {
-    title: "Cassette Empty",
-    atmID: "ATM-GB-0003",
-    count: 4,
-    updatedTime: "2 mins ago"
-  },
-  {
-    title: "Cash Jam Detected",
-    atmID: "ATM-GB-0007",
-    count: 2,
-    updatedTime: "5 mins ago"
-  },
-  {
-    title: "Memory Spike",
-    atmID: "ATM-GB-0005",
-    count: 6,
-    updatedTime: "11 mins ago"
-  },
-  {
-    title: "Cassette Empty",
-    atmID: "ATM-GB-0002",
-    count: 5,
-    updatedTime: "24 mins ago"
-  },
-  {
-    title: "Cash Jam Detected",
-    atmID: "ATM-GB-0013",
-    count: 1,
-    updatedTime: "37 mins ago"
-  }
-];
+    {title: "Cassette Empty", atmID: "ATM-GB-0003", count: 4, updatedTime: "2 mins ago"},
+    {title: "Cash Jam Detected", atmID: "ATM-GB-0007", count: 2, updatedTime: "5 mins ago"},
+    {title: "Memory Spike", atmID: "ATM-GB-0005", count: 6, updatedTime: "11 mins ago"},
+    {title: "Cassette Empty", atmID: "ATM-GB-0002", count: 5, updatedTime: "24 mins ago"},
+    {title: "Cash Jam Detected", atmID: "ATM-GB-0013", count: 1, updatedTime: "37 mins ago"}
+  ];
 
   const searchedAnomalies = anomalyList.filter((a) =>
     a.title.toLowerCase().includes(search.toLowerCase())
@@ -69,17 +43,17 @@ function Dashboard() {
         {/* Title and Count */}
         <div className="titleContainer">
           <h1>Anomalies Detected</h1>
-          <h2>[count]</h2>
+          <h2>{searchedAnomalies.length}</h2>
         </div>
 
-        {/* Example Anomaly Cards - Searchable */}
+        {/* Anomaly Cards */}
         <div className="anomalyContainer">
           {searchedAnomalies.map((a, index) => (
             <AnomalyCard
               key={index}
               title={a.title}
               atmID={a.atmID}
-              count={`${a.count} anomaly${a.count > 1 ? "ies" : ""}`}
+              count={`${a.count} ${a.count === 1 ? "anomaly" : "anomalies"}`}
               updatedTime={`Updated: ${a.updatedTime}`}
             />
           ))}
