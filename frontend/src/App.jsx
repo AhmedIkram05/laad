3dff735e8490d25c9bd95011aafbe0d88064d3a0
2579986 <149397608+2579986@users.noreply.github.com>
2026-03-25 21:32:34 +0000

    fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 /* Import Pages */
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import Starred from "./pages/Starred";
+import Completed from "./pages/Completed";
+import AnomalyData from "./pages/AnomalyData";
 
 
 function App() {
@@ -18,6 +21,9 @@ function App() {
         {/* Set Main Layout to Pages */}
         <Route element={<MainLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/starred" element={<Starred />} />
+          <Route path="/completed" element={<Completed />} />
+          <Route path="/data" element={<AnomalyData />} />
         </Route>
 
       </Routes>
