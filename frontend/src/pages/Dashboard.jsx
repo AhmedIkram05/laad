c56c7430b1ae91861910a78a929b8402e47ac315
2579986 <149397608+2579986@users.noreply.github.com>
2026-03-25 14:03:58 +0000

    created example anomaly card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,9 @@
+/* Import Components */
+import StarIcon from "../components/StarIcon";
+
+/* Import Icons */
+import { GoAlert } from "react-icons/go";
+
 /* Import Styles */
 import './Dashboard.css'
 
@@ -13,10 +19,40 @@ function Dashboard() {
 
       {/* Main Page Content */}
       <div className="mainContainer">
-
-        <div>
+ 
+        {/* Title and Count */}
+        <div className="titleContainer">
           <h1>Anomalies Detected</h1>
-          <p>Placeholder text.</p>
+          <h2>[count]</h2>
+        </div>
+
+        {/* Anomaly Cards */}
+        <div className="anomalyContainer">
+
+          <div className="anomalyCard">
+
+            <div className="cardDetails">
+
+              <div className="alert">
+                <div className="alertIcon"><GoAlert /></div>
+                <h3>Cassette Empty</h3>
+              </div>
+
+              <div className="minorDetails">
+                <p>ATM-GB-0003</p>
+                <div className="detailsContainer">
+                  <p>4 anomalies</p>
+                  <p>Updated: 2 mins ago</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="cardButtons">
+              <button className="viewButton">View</button>
+              <StarIcon />
+            </div>
+          </div>
+
         </div>
 
       </div>
